dcc917df1783c1a66f5a84f86a5ba64b144f5eac
pokzcodes <98141051+pokzcodes@users.noreply.github.com>
2022-03-15 19:04:21 -0400

    attempt to fix double tele

--- src/char/i_char.py
+++ src/char/i_char.py
@@ -288,7 +288,6 @@ class IChar:
             mouse.move(pos_monitor[0], pos_monitor[1], randomize=2, delay_factor=[factor*0.1, factor*0.14])
             wait(0.02, 0.03)
             mouse.click(button="right")
-            wait(0.02, 0.03)
             mouse.release(button="right")
             wait(self._cast_duration, self._cast_duration + 0.02)
         else:
