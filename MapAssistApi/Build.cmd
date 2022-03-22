0fd5c4739d1a3fe25e92054579be41289c6c347c
jagarop <cfigueiroa@gmail.com>
2022-03-22 01:13:57 -0300

    Update Build.cmd

--- MapAssistApi/Build.cmd
+++ MapAssistApi/Build.cmd
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\MSBuild.exe" MapAssist.sln /p:OutDir=../botty-r/binary /p:Configuration=Release /t:MapAssist:Rebuild
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\MSBuild.exe" MapAssist.sln /p:OutDir=bin\x64\Release /p:Configuration=Release /t:MapAssist:Rebuild
