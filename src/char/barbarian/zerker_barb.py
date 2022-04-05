bf39a974e6f4f3620061df9e2f876f98c639a818
pokzcodes <98141051+pokzcodes@users.noreply.github.com>
2022-04-05 17:35:24 -0400

    Update zerker_barb.py

--- src/char/barbarian/zerker_barb.py
+++ src/char/barbarian/zerker_barb.py
@@ -192,7 +192,7 @@ class ZerkerBarb(Barbarian):
                             self._pather.traverse(reposition_pos, self, time_out = 3.0)
                             last_move = time.time()
                         else:
-                            while monster["dist"] > 3.0 and time.time() - monster_start < 5.0:
+                            while monster and monster["dist"] > 3.0 and time.time() - monster_start < 5.0:
                                 Logger.debug(f"    Monster {monster['id']} distance is too far ({round(monster['dist'], 2)}), moving closer...")
                                 self._pather.move_to_monster(self, monster)
                                 last_move = time.time()
