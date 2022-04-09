540bd901a3ccdaa7dcecdb637df3a1c561c0493a
pokzcodes <98141051+pokzcodes@users.noreply.github.com>
2022-04-09 02:24:56 -0400

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -415,8 +415,8 @@ class Bot:
         self._no_stash_counter += 1
         if not self._picked_up_items and (self._no_stash_counter > 4 or self._pick_corpse):
             self._no_stash_counter = 0
-            if is_loading: is_loading = self._ui_manager.wait_for_loading_finish()
             force_stash = self._ui_manager.should_stash()
+        
         # Stash stuff, either when item was picked up or after X runs without stashing because of unwanted loot in inventory
         if self._picked_up_items or force_stash:
             # Check config/gold and see if we need to enable/disable gold pickup
