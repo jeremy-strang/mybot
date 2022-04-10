f75daaa772eff1cd8fd6b0af9a7847906f8ad7ca
pokzcodes <98141051+pokzcodes@users.noreply.github.com>
2022-04-10 13:27:23 -0400

    update eld/shenk/pindle for zerker

--- src/char/barbarian/zerker_barb.py
+++ src/char/barbarian/zerker_barb.py
@@ -43,6 +43,56 @@ class ZerkerBarb(Barbarian):
         keyboard.release(self._char_config["stand_still"])
         wait(0.03, 0.04)
 
+    def _kill_mobs(self,
+                  prioritize: list[MonsterRule],
+                  ignore: list[MonsterRule] = None,
+                  time_out: float = 40.0,
+                  boundary: Tuple[int, int, int, int] = None,
+                  reposition_pos = None,
+                  reposition_time: float = 6.0,
+                  do_howl: bool=False
+                ) -> bool:
+        start = time.time()
+        last_move = start
+        elapsed = 0
+        monsters = sort_and_filter_monsters(self._api.data, prioritize, ignore, boundary, ignore_dead=True)
+        if len(monsters) == 0: return True
+        Logger.debug(f"Beginning combat against {len(monsters)}...")
+        while elapsed < time_out and len(monsters) > 0:
+            data = self._api.get_data()
+            if data:
+                for monster in monsters:
+                    monster = self._api.find_monster(monster["id"])
+                    if monster:
+                        monster_start = time.time()
+                        if time.time() - last_move > reposition_time and reposition_pos is not None:
+                            Logger.debug("    Stood in one place too long, repositioning")
+                            self._pather.traverse(reposition_pos, self, time_out = 3.0)
+                            last_move = time.time()
+                        else:
+                            while monster and monster["dist"] > 3.0 and time.time() - monster_start < 5.0:
+                                Logger.debug(f"    Monster {monster['id']} distance is too far ({round(monster['dist'], 2)}), moving closer...")
+                                self._pather.move_to_monster(self, monster)
+                                last_move = time.time()
+                                monster = self._api.find_monster(monster["id"])
+                                if do_howl and monster and monster["dist"] <= 3.0:
+                                    self.cast_howl()
+                            if monster and monster["dist"] <= 3.0 and not self.tele_stomp_monster("berserk", 3.0, monster, max_distance=3):
+                                wait(0.1)
+            wait(0.1)
+            monsters = sort_and_filter_monsters(self._api.data, prioritize, ignore, boundary, ignore_dead=True)
+            elapsed = time.time() - start
+        self.post_attack()
+        Logger.debug(f"    Finished killing mobs, combat took {elapsed} sec")
+        return True
+
+    def _kill_superunique(self, name: str = None) -> bool:
+        rules = [MonsterRule(names=[name])]
+        if type(name) is str:
+            rules.append(MonsterRule(names=[name]))
+        self._kill_mobs(rules, time_out=20, do_howl=True)
+        return True
+    
     def kill_uniques(self, pickit=None, time_out: float=15.0, looted_uniques: set=set(), boundary=None) -> bool:
         Logger.debug(f"Beginning combat")
         rules = [
@@ -96,7 +146,10 @@ class ZerkerBarb(Barbarian):
         return True
 
     def kill_mephisto(self) -> bool:
-        rules = [MonsterRule(names=["Mephisto"])]
+        rules = [
+            MonsterRule(names=["Mephisto"]),
+            MonsterRule(monster_types=[MonsterType.SUPER_UNIQUE]),
+        ]
         self._kill_mobs(rules, time_out=40)
         return True
 
@@ -112,67 +165,13 @@ class ZerkerBarb(Barbarian):
         return True
 
     def kill_summoner(self) -> bool:
-        rules = [
-            MonsterRule(names=["Summoner"]),
-            MonsterRule(monster_types = [MonsterType.SUPER_UNIQUE]),
-        ]
-        self._kill_mobs(rules, time_out=20, do_howl=True)
-        return True
+        return self._kill_superunique("Summoner")
+
+    def kill_pindle(self) -> bool:
+        return self._kill_superunique("Pindleskin")
 
     def kill_nihlathak(self) -> bool:
-        rules = [
-            MonsterRule(names=["Nihlathak"]),
-            MonsterRule(monster_types = [MonsterType.SUPER_UNIQUE]),
-        ]
-        self._kill_mobs(rules, time_out=20, do_howl=True)
-        return True
+        return self._kill_superunique("Nihlathak")
 
     def kill_countess(self) -> bool:
-        rules = [
-            MonsterRule(monster_types = [MonsterType.SUPER_UNIQUE]),
-        ]
-        self._kill_mobs(rules, time_out=20, do_howl=True)
-        return True
-
-    def _kill_mobs(self,
-                  prioritize: list[MonsterRule],
-                  ignore: list[MonsterRule] = None,
-                  time_out: float = 40.0,
-                  boundary: Tuple[int, int, int, int] = None,
-                  reposition_pos = None,
-                  reposition_time: float = 6.0,
-                  do_howl: bool=False
-                ) -> bool:
-        start = time.time()
-        last_move = start
-        elapsed = 0
-        monsters = sort_and_filter_monsters(self._api.data, prioritize, ignore, boundary, ignore_dead=True)
-        if len(monsters) == 0: return True
-        Logger.debug(f"Beginning combat against {len(monsters)}...")
-        while elapsed < time_out and len(monsters) > 0:
-            data = self._api.get_data()
-            if data:
-                for monster in monsters:
-                    monster = self._api.find_monster(monster["id"])
-                    if monster:
-                        monster_start = time.time()
-                        if time.time() - last_move > reposition_time and reposition_pos is not None:
-                            Logger.debug("    Stood in one place too long, repositioning")
-                            self._pather.traverse(reposition_pos, self, time_out = 3.0)
-                            last_move = time.time()
-                        else:
-                            while monster and monster["dist"] > 3.0 and time.time() - monster_start < 5.0:
-                                Logger.debug(f"    Monster {monster['id']} distance is too far ({round(monster['dist'], 2)}), moving closer...")
-                                self._pather.move_to_monster(self, monster)
-                                last_move = time.time()
-                                monster = self._api.find_monster(monster["id"])
-                                if do_howl and monster and monster["dist"] <= 3.0:
-                                    self.cast_howl()
-                            if monster and monster["dist"] <= 3.0 and not self.tele_stomp_monster("berserk", 3.0, monster, max_distance=3):
-                                wait(0.1)
-            wait(0.1)
-            monsters = sort_and_filter_monsters(self._api.data, prioritize, ignore, boundary, ignore_dead=True)
-            elapsed = time.time() - start
-        self.post_attack()
-        Logger.debug(f"    Finished killing mobs, combat took {elapsed} sec")
-        return True
+        return self._kill_superunique("Countess")
